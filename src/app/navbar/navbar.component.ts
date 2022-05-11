8cb5c0821d3f47bcf0786fb5b24c7957fe14b1ce
Gergely Pap <pgege42@gmail.com>
2022-05-11 12:03:39 +0200

    add navbar links dynamically

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Route } from '@angular/router';
+
+interface Link {
+  path: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  links: Link[] = [
+    { path: '/top', text: 'top' },
+    { path: '/latest', text: 'latest' },
+    { path: '/best', text: 'best' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
