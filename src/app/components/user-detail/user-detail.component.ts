d4d227107762f2a1ae23bb18ca943696ab98dc21
Gergely Pap <pgege42@gmail.com>
2022-05-21 15:53:55 +0200

    use async pipe in user-detail

--- src/app/components/user-detail/user-detail.component.ts
+++ src/app/components/user-detail/user-detail.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Item } from 'src/app/models/item.model';
 import { User } from 'src/app/models/user.model';
 import { ApiService } from '@services/api.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-detail',
@@ -10,15 +10,12 @@ import { ApiService } from '@services/api.service';
   styleUrls: ['./user-detail.component.css'],
 })
 export class UserDetailComponent implements OnInit {
-  user: User | undefined;
-  submitted: Item[] = [];
+  user$!: Observable<User>;
 
   constructor(private apiService: ApiService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     const { username } = this.route.snapshot.params;
-    this.apiService.getUser(username).subscribe((user) => {
-      this.user = user;
-    });
+    this.user$ = this.apiService.getUser(username);
   }
 }
